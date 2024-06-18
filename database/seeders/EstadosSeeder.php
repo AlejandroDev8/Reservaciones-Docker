73be2ed2c6351a72eb8990b18d32d97a7d5f040d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 18:07:16 -0600

    Creando los Seeders para los acomodos, estados y salas

---
+++ database/seeders/EstadosSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EstadosSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('estados')->insert([
+            'estados' => 'Pendiente',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('estados')->insert([
+            'estados' => 'Aceptada',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('estados')->insert([
+            'estados' => 'Rechazada',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
