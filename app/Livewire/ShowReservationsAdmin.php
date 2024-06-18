1caa49efc5da7d33a38f610dc521ea47c9d5d838
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 20:06:22 -0600

    Creando componentes de livewire para mostrar las reservaciones, dependiendo si es usario o admin

---
+++ app/Livewire/ShowReservationsAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowReservationsAdmin extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-reservations-admin');
+    }
+}
