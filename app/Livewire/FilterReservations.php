1b9ff4fbb3a830f76a295127beca6ac2875f59aa
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 21:23:33 -0600

    Agregando el componente FilterReservations para filtrar reservaciones

--- app/Livewire/FilterReservations.php
+++ app/Livewire/FilterReservations.php
@@ -2,12 +2,46 @@
 
 namespace App\Livewire;
 
+use App\Models\Sala;
+use App\Models\User;
+use App\Models\Acomodo;
+use App\Models\Estados;
 use Livewire\Component;
 
 class FilterReservations extends Component
 {
+    public $sala;
+    public $user;
+    public $estado;
+
+    public function filtrarSolicitudes()
+    {
+        // Verificar si los campos están seleccionados
+        if ($this->sala == "--Seleccione--") {
+            $this->sala = null;
+        }
+        if ($this->user == "--Seleccione--") {
+            $this->user = null;
+        }
+        if ($this->estado == "-- Seleccione --") {
+            $this->estado = null;
+        }
+
+        $this->dispatch('terminosBusqueda', $this->sala, $this->user, $this->estado);
+    }
+
     public function render()
     {
-        return view('livewire.filter-reservations');
+        $salas = Sala::all();
+        $acomodos = Acomodo::all();
+        $users = User::all();
+        $estados = Estados::all();
+
+        return view('livewire.filter-reservations', [
+            'salas' => $salas,
+            'acomodos' => $acomodos,
+            'users' => $users,
+            'estados' => $estados
+        ]);
     }
 }
