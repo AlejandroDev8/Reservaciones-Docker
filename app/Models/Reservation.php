d336ca9e4515484b9e2662e6b42835acd8ba7814
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 20:09:43 -0600

    Actualizando el modelo de reservación y creando métodos de relación

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Relación con la tabla Sala
+    public function sala()
+    {
+        return $this->belongsTo(Sala::class, 'sala_id');
+    }
+
+    public function estados()
+    {
+        return $this->belongsTo(Estados::class, 'estado_id');
+    }
+
+    public function acomodo()
+    {
+        return $this->belongsTo(Acomodo::class, 'acomodo_id');
+    }
 }
