d75a3762ec033c014c2de5969928fe5669e7b4fe
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 21:04:14 -0600

    Agregando funcionalidad de búsqueda al componente ShowReservationsAdmin

--- app/Livewire/ShowReservationsAdmin.php
+++ app/Livewire/ShowReservationsAdmin.php
@@ -3,11 +3,32 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Reservation;
 
 class ShowReservationsAdmin extends Component
 {
+    public $sala;
+    public $user;
+    public $estado;
+
+    protected $listeners = ['searchTerms' => 'search'];
+
+    public function search($sala, $user, $estado)
+    {
+        $this->sala = $sala;
+        $this->user = $user;
+        $this->estado = $estado;
+    }
+
     public function render()
     {
-        return view('livewire.show-reservations-admin');
+        $solicitudes = Reservation::where('sala_id', 'like', '%' . $this->sala . '%')
+            ->where('user_id', 'like', '%' . $this->user . '%')
+            ->where('estado_id', 'like', '%' . $this->estado . '%')
+            ->get();
+
+        return view('livewire.show-reservations-admin', [
+            'solicitudes' => $solicitudes,
+        ]);
     }
 }
