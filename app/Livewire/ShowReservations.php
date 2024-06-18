5090fd7b7a1c5f616156ed75ba00e848c86f654c
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-06-17 21:03:46 -0600

    Actualizando el componente ShowReservations para incluir la paginación y la funcionalidad de eliminación.

--- app/Livewire/ShowReservations.php
+++ app/Livewire/ShowReservations.php
@@ -3,11 +3,23 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Reservation;
 
 class ShowReservations extends Component
 {
+    protected $listeners = ['deleteReservation'];
+
+    public function deleteReservation(Reservation $reservation)
+    {
+        $reservation->delete();
+    }
+
     public function render()
     {
-        return view('livewire.show-reservations');
+        $solicitudes = Reservation::where('user_id', auth()->user()->id)->paginate(10);
+
+        return view('livewire.show-reservations', [
+            'solicitudes' => $solicitudes,
+        ]);
     }
 }
